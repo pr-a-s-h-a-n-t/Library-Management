6faff37305209937bfeac264052ab414e739b818
Prashant Mishra <prashantmishra111@outlook.com>
2023-03-21 03:54:11 +0530

    dummy data added!

--- client/index.js
+++ client/index.js
@@ -2,4 +2,51 @@ console.log("hii ✋, I am prashant!");
 
 let wrapper = document.getElementById("cards_wrapper");
 
+//  data
+
  
+    let hotels =  [
+      {
+        "name": "Grand Hotel",
+        "location": "New York",
+        "rooms": [
+          {
+            "room_type": "Single",
+            "price_per_night": 100,
+            "available_dates": ["2023-04-01", "2023-04-02", "2023-04-03"]
+          },
+          {
+            "room_type": "Double",
+            "price_per_night": 150,
+            "available_dates": ["2023-04-01", "2023-04-03", "2023-04-04"]
+          },
+          {
+            "room_type": "Suite",
+            "price_per_night": 300,
+            "available_dates": ["2023-04-01", "2023-04-02", "2023-04-04"]
+          }
+        ]
+      },
+      {
+        "name": "Beach Resort",
+        "location": "Miami",
+        "rooms": [
+          {
+            "room_type": "Single",
+            "price_per_night": 80,
+            "available_dates": ["2023-04-01", "2023-04-03", "2023-04-04"]
+          },
+          {
+            "room_type": "Double",
+            "price_per_night": 120,
+            "available_dates": ["2023-04-01", "2023-04-02", "2023-04-04"]
+          },
+          {
+            "room_type": "Suite",
+            "price_per_night": 250,
+            "available_dates": ["2023-04-02", "2023-04-03", "2023-04-04"]
+          }
+        ]
+      }
+    ]
+  
