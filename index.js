c241aa4aedbfdda1571c63d45f81c4d0d4a61293
Prashant Mishra <prashantmishra111@outlook.com>
2023-04-02 04:14:45 +0530

    validator package installed, data validation  logic implemented!

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 
 const clc = require("cli-color");
+const { Model } = require("mongoose");
 
 // File imports--
+const { cleanUpAndValidate } = require("./utils/AuthUtils");
 
 // Variables--
 
@@ -11,10 +13,16 @@ const app = express();
 
 const PORT = process.env.PORT || 8000; // after deploying the port which is freely available will be automatically assigned!!
 
+
 // ejs(view engine) // it will search the files inside the view
 // folder  then it  will render You don't have to import anything
 
 app.set("view engine", "ejs");
+
+// middlewares
+//remember we have to use middleware because by default the data is url-encoded format so we need to type cast into the json formate
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes(Note- urls are not case  sensitive they will be converted to smaller case)--
 app.get("/", (req, res) => {
@@ -30,14 +38,81 @@ app.get("/login", (req, res) => {
 });
 
 // end point for signup and login page to post the data to the server!!
+//remember we have to use middleware because by default the data is url-encoded format so we need to type cast into the json formate
+//
 
-app.post("/signup", (req, res) => {
-  console.log(res.body);
-  return res.send(true);
+//MVC 
+// Model- functions which interact with database
+// utility functions- functions which does not interact with db
+app.post("/signup", async(req, res) => {
+  console.log(req.body);
+  const { name, email, password, username } = req.body;
+
+  try {
+    await cleanUpAndValidate({ name, email, password, username });
+
+    //check if the user exits
+
+    const userExistEmail = await userSchema.findOne({ email });
+
+    console.log(userExistEmail);
+    if (userExistEmail) {
+      return res.send({
+        status: 400,
+        message: "Email Already exits",
+      });
+    }
+
+    const userExistUsername = await userSchema.findOne({ username });
+
+    if (userExistUsername) {
+      return res.send({
+        status: 400,
+        message: "Username Already exits",
+      });
+    }
+
+    //hash the password using bcypt
+    const hashPassword = await bcrypt.hash(password, saltRound);
+
+    const user = new userSchema({
+      name: name,
+      email: email,
+      password: hashPassword,
+      username: username,
+    });
+
+    try {
+      const userDb = await user.save();
+      console.log(userDb);
+      return res.send({
+        status: 201,
+        message: "User register successfully",
+        data: userDb,
+      });
+    } catch (error) {
+      return res.send({
+        status: 500,
+        message: "Database error",
+        error: error,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: 400,
+      message: "Data Invalid",
+      error: error,
+    });
+  }
 });
 
+//data validations
+ 
+ 
+
 app.post("/login", (req, res) => {
-  console.log(res.body);
+  console.log(req.body);
   return res.send(true);
 });
 
@@ -50,7 +125,8 @@ app.listen(PORT, () => {
 
 //EGS
 // step1 create server and connect to mongodb database!.
-// Step2 SignUP
+// Step2 SignUP( 1.data validation/cleanup, 2.firt check  user exits or not  
+// if not then create a user in db,)
 // Step3 Email verification ...
 // Step4 login
 // after login redirect to dashboard!.
